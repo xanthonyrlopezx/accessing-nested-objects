8aef89fcf00273483aa5bc1320e036443423c67b
Anthony <72381482+xanthonyrlopezx@users.noreply.github.com>
2022-10-15 21:03:12 -0700

    first commit

---
+++ accessing-nested-objects.js
@@ -0,0 +1,14 @@
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = undefined;
+var gloveBoxContents = myStorage.car.inside["glove box"];
